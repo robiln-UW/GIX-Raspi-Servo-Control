024f135312046a2e7ece896519b8ab24a38ce6c7
robiln-UW <robiln@uw.edu>
2018-07-16 04:10:16 +0000

    Added arduino files. Added to README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# GIX-Raspi-Servo-Control
+X-Raspi-Servo-Control
+
+This repository contains code for controlling servos with a Raspberry Pi. It is intended to be used for the GIX Summer Robotics program. It contains code for both Raspberry Pi and Arduino. The Raspberry Pi sends serial commands over USB to the Arduino, which controls the servo via PWM.
+
+
+[raspi](raspi) - python code to be run on the raspberry pi
+
+[arduino](arduino) - .ino files to be run on the arduino 
+
---
+++ arduino/servo_control/servo_control.ino
@@ -0,0 +1,40 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 90;    // variable to store the servo position
+char readByte;
+String readBuffer = "000";
+
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.write(pos);
+}
+
+void loop() {
+
+  for(int i = 0; i < 3; i++)
+  {
+    while(Serial.available() < 1) 
+    {
+    }
+    readBuffer[i] = Serial.read();
+  }
+  Serial.println(readBuffer);
+  myservo.write(readBuffer.toInt());
+
+}
+
+
+
